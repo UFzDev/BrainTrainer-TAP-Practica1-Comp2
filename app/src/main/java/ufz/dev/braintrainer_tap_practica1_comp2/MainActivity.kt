f78ba7d9e4af2ed15601ae0e2d7221956dd9a398
UFzDev <urielfloreszarate005@gmail.com>
2026-04-28 22:15:29 -0600

    Menu Hamburguesa

--- app/src/main/java/ufz/dev/braintrainer_tap_practica1_comp2/MainActivity.kt
+++ app/src/main/java/ufz/dev/braintrainer_tap_practica1_comp2/MainActivity.kt
@@ -3,15 +3,40 @@ package ufz.dev.braintrainer_tap_practica1_comp2
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+
+        toolbar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_exit -> {
+                    finishAffinity()
+                }
+                R.id.nav_game, R.id.nav_history, R.id.nav_settings -> {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                }
+            }
+            true
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(drawerLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
